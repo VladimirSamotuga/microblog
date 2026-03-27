1345c4518e0b588590abda4e03e0baf0f59f70f3
VladimirSamotuga <samotuga.vladi@gmail.com>
2026-03-27 16:48:16 +0300

    27.3

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
@@ -12,6 +14,8 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+mail = Mail(app)
+bootstrap = Bootstrap(app)
 login.login_view = 'login'
 
 from app import routes, models, errors
