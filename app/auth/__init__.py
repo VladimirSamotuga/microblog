65944c21f30ed9f9a5a6c0917d5735eea4713a82
VladimirSamotuga <samotuga.vladi@gmail.com>
2026-04-15 17:06:42 +0300

    15.04

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-bp = Blueprint('errors', __name__)
+bp = Blueprint('auth', __name__)
 
-from app.errors import handlers
+from app.auth import routes
--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
+from app.auth.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User, Post
 from urllib.parse import urlparse
